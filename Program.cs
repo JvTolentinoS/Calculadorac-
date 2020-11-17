3b71f21557b96cdc08442d528a38d9e39e2156ac
joao <joaovtolente@gmail.com>
2020-11-17 09:02:34 -0300

    Calculadora

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Calculadorac_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a operação que deseje fazer \nSoma, Subtração, Multiplicação, Divisão");
+            string operacao = Console.ReadLine();
+
+            Console.WriteLine("Digite o 1º número");
+            float num1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o 2º número");
+            float num2 = float.Parse(Console.ReadLine());
+
+            float resultado = 0;
+
+            switch(operacao){
+
+                case "Soma":
+                    resultado = num1 + num2;
+                    break;
+
+                case "Subtracao":
+                    resultado = num1 - num2;
+                    break;
+
+                case "Multiplicacao":
+                    resultado = num1 * num2;
+                    break;
+
+                case "Divisao":
+                    resultado = num1 / num2;
+                    break;
+                default:
+                    Console.WriteLine("Operação Inválida");
+                    break;            
+            }
+            Console.WriteLine($"Calculo: {num1} e {num2} = Resultado : {resultado}");
+        }
+    }
+}
